5c630bba1c829f83f02e3abb192601a9529d40ee
brenda zaya <brendazayait@gmail.com>
2022-07-10 02:08:18 -0300

    conectando json

--- index.js
+++ index.js
@@ -1,174 +1,4 @@
 
-// arrays de productos
-
-    const productos = [
-        {
-            id: 1,
-            nombre: "IPA",
-            amargor: "ALTO",
-            ibu: "82",
-            aroma: "CITRICA",
-            ABV: "6,5%",
-            tamaño: "500 cc",
-            precio: 260
-        },
-        
-        {
-            id: 2,
-            nombre: "KOLSCH",
-            amargor: "MODERADO",
-            ibu: "21",
-            aroma: "FRUTADA",
-            ABV: "4,5%",
-            tamaño: "500 cc",
-            precio: 220
-        },
-        
-        {
-            id: 3,
-            nombre: "STOUT",
-            amargor: "MEDIO",
-            ibu: "42",
-            aroma: "TOSTADA",
-            ABV: "6,5%",
-            tamaño: "500 cc",
-            precio: 240
-        },
-        
-        {
-            id: 4,
-            nombre: "RED IPA",
-            amargor: "ALTO",
-            ibu: "70",
-            aroma: "CITRICA",
-            ABV: "6,0%",
-            tamaño: "500 cc",
-            precio: 260
-        },
-        
-        {
-            id: 5,
-            nombre: "BARLEY WINE",
-            amargor: "MEDIO",
-            ibu: "54",
-            aroma: "CARAMELO",
-            ABV: "11%",
-            tamaño: "500 cc",
-            precio: 240
-        },
-
-        {
-            id: 6,
-            nombre: "PORTER",
-            amargor: "BAJO",
-            ibu: "32",
-            aroma: "CHOCOLATE",
-            ABV: "6,5%",
-            tamaño: "500 cc",
-            precio: 240
-        },
-
-        {
-            id: 7,
-            nombre: "HONEY",
-            amargor: "BAJO",
-            ibu: "20",
-            aroma: "CITRICA",
-            ABV: "6%",
-            tamaño: "500 cc",
-            precio: 220
-        },
-
-        {
-            id: 8,
-            nombre: "RED HONEY",
-            amargor: "BAJO",
-            ibu: "15",
-            aroma: "ACARAMELADO",
-            ABV: "4,5%",
-            tamaño: "500 cc",
-            precio: 220
-        },
-        
-        {
-            id: 9,
-            nombre: "IRISH",
-            amargor: "MEDIO",
-            ibu: "24",
-            aroma: "MALTA",
-            ABV: "5,3%",
-            tamaño: "500 cc",
-            precio: 220
-        },
-        
-        {
-            id: 10,
-            nombre: "GOLDEN ALE",
-            amargor: "BAJO",
-            ibu: "22",
-            aroma: "FRUTAL",
-            ABV: "5%",
-            tamaño: "500 cc",
-            precio: 220
-        },
-        
-        {
-            id: 11,
-            nombre: "APA",
-            amargor: "MEDIO",
-            ibu: "34",
-            aroma: "LÚPULO",
-            ABV: "5,5%",
-            tamaño: "500 cc",
-            precio: 240
-        },
-        
-        {
-            id: 12,
-            nombre: "CREAM STOUT",
-            amargor: "MODERADO",
-            ibu: "32",
-            aroma: "CAFÉ",
-            ABV: "8%",
-            tamaño: "500 cc",
-            precio: 260
-        },
-        
-        {
-            id: 13,
-            nombre: "NEIPA",
-            amargor: "ALTO",
-            ibu: "62",
-            aroma: "LÚPULO",
-            ABV: "8,5%",
-            tamaño: "500 cc",
-            precio: 260
-        },
-
-        {
-            id: 14,
-            nombre: "AMERICAN STOUT",
-            amargor: "MEDIO",
-            ibu: "42",
-            aroma: "CANELA",
-            ABV: "6%",
-            tamaño: "500 cc",
-            precio: 240
-        },
-
-        {
-            id: 15,
-            nombre: "PILSEN",
-            amargor: "BAJO",
-            ibu: "18",
-            aroma: "MALTA",
-            ABV: "5%",
-            tamaño: "500 cc",
-            precio: 220
-        }
-    ]
-    
-
     // Validar edad
 
     function validarEdad() {
@@ -179,100 +9,121 @@
         }
     }
 
-    // simulacion de compra
 
-    function simuladorCompra() {
-        let total = 0;
-        let seguirComprando = '';
-        do {
-            let nombreProducto = prompt("¿Que cerveza querés comprar? (honey, ipa, golden, pilsen, barley wine, kolch, neipa, porter, irish, etc)").toUpperCase();
+const contenedorProductos = document.querySelector('.contenedorProductos');
 
-            let cantidad = parseInt(prompt (`¿Cuantas latas de ${nombreProducto} querés agregar al carrito?`));
+// Conector con json
 
-            let producto = productos.find(elemento => elemento.nombre === nombreProducto);
+try {
+    fetch('../api.json')
+    .then(respuesta => {
+        return respuesta.json()
+    })
+    .then(datos => {
+        let contador = 0;
+        while (datos.length > contador) {
 
-            let subtotal = producto.precio * cantidad;
+            // Creacion de cards para productos
 
-            total = total + subtotal;
+            let producto = document.createElement('div');
 
-            seguirComprando = prompt("¿Querés seguir comprando?").toUpperCase();
+            let tituloProducto = document.createElement('div');
 
-        } while (seguirComprando === "SI");
+            let iconoContainer = document.createElement('div');
+            let icono = document.createElement('img');
 
-        alert("El precio total a pagar es de: $ " + total);
-        
-    }
+            let nombreProducto = document.createElement('h2');
 
-    // buscador
+            let imgContenedor = document.createElement('div');
+            let img = document.createElement('img');
 
-    function buscador() {
-        let buscar = prompt("¿Qué cerveza estás buscando?").toUpperCase();
+            let infoProducto = document.createElement('div');
+            let especificaciones = document.createElement('div');
 
-        let resultadosDeBusqueda = productos.filter(elemento => elemento.nombre.includes(buscar));
-        console.log(resultadosDeBusqueda);
-    }
-    
-    // filtro
+            let ul = document.createElement('ul');
+            let liIbu = document.createElement('li');
+            let liAbv = document.createElement('li');
+            let liCc = document.createElement('li');
 
-    function filtro() {
-        let filtrar = prompt("¿Desea filtrar resultados?").toUpperCase(); 
+            let botonAgregar = document.createElement('buton');
+            let precioBoton = document.createElement('p');
+            let agregarAlCarrito = document.createElement('p');
+            let descripcionProducto = document.createElement('div')
+            let textoDescriptivo = document.createElement('p');
 
-        if (filtrar === "SI") {
+            // asignación de clases
 
-            let asignarFiltro = prompt("¿Que filtro querés aplicar (Nombre, Precio, amargor)").toUpperCase();
+            producto.setAttribute('class', 'producto');
 
-            if (asignarFiltro === "NOMBRE") {
-                let filtro = prompt("Ingrese nombre").toUpperCase();
+            tituloProducto.setAttribute('class', 'tituloProducto');
+            iconoContainer.setAttribute('class', 'iconoContainer');
+            icono.setAttribute('class', 'icono');
+            icono.setAttribute('src', datos[contador].icono);
+            nombreProducto.setAttribute('class', 'nombreProducto');
 
-                let resultadosDeFiltro = productos.filter((elemento) => elemento.nombre === filtro);
-                console.log(resultadosDeFiltro);
+            imgContenedor.setAttribute('class', 'imgContenedor');
+            img.setAttribute('src', datos[contador].img);
 
-            } else if (asignarFiltro === "PRECIO") {
-                let filtro = parseInt(prompt("Ingrese precio maximo"));
+            infoProducto.setAttribute('class', 'infoProducto');
 
-                let resultadosDeFiltro = productos.filter((elemento) => elemento.precio <= filtro);
-                console.log(resultadosDeFiltro);
+            especificaciones.setAttribute('class', 'especificaciones');
+            ul.setAttribute('class', 'ul');
+            liIbu.setAttribute( 'class', 'ibu');
+            liAbv.setAttribute('class', 'abv');
+            liCc.setAttribute( 'class', 'cc');
 
-            } else if (asignarFiltro === "AMARGOR") {
-                let filtro = prompt("ingrese amargor deseado (bajo, medio, moderado, alto)").toUpperCase();
+            botonAgregar.setAttribute('class', 'botonAgregar');
+            precioBoton.setAttribute('class', 'precio');
+            agregarAlCarrito.setAttribute('class', 'agregarAlCarrito');
 
-                let resultadosDeFiltro = productos.filter((elemento) => elemento.amargor === filtro);
-                console.log(resultadosDeFiltro);
-            } else {
-                alert("El filtro ingresado es invalido");
-            }
+            descripcionProducto.setAttribute('class', 'descripcionProducto');
+            textoDescriptivo.setAttribute('class', 'textoDescriptivo')
+
+            // Completar información de las cars con los productos json
+
+            nombreProducto.innerHTML = datos[contador].nombre;
+            liIbu.innerHTML = "IBU: " + datos[contador].ibu;
+            liAbv.innerHTML = "ABV: " + datos[contador].ABV;
+            liCc.innerHTML = datos[contador].tamaño;
+            agregarAlCarrito.innerHTML = "Agregar al carrito";
+            precioBoton.innerHTML = "$ " + datos[contador].precio;
+
+            textoDescriptivo.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, at recusandae!";
+            
+            // ordenado de las etiquetas (padres e hijos)
+            contenedorProductos.appendChild(producto);
+            
+            producto.appendChild(tituloProducto);
+            producto.appendChild(imgContenedor);
+            producto.appendChild(infoProducto);
+
+            tituloProducto.appendChild(iconoContainer);
+            tituloProducto.appendChild(nombreProducto);
+
+            iconoContainer.appendChild(icono);
+
+            imgContenedor.appendChild(img);
+
+            infoProducto.appendChild(especificaciones);
+            infoProducto.appendChild(botonAgregar);
+            infoProducto.appendChild(descripcionProducto);
+
+            especificaciones.appendChild(ul);
+            ul.appendChild(liIbu);
+            ul.appendChild(liAbv);
+            ul.appendChild(liCc);
+
+            botonAgregar.appendChild(precioBoton);
+            botonAgregar.appendChild(agregarAlCarrito);
+
+            descripcionProducto.appendChild(textoDescriptivo);
+
+            contador++;
         }
-    }
-
-    // DOM saludo
-
-    function validarEdad() {
-        if (confirm("Para poder acceder a este contenido, necesitamos validar tu edad. ¿Sos mayor de 18?") == true) {
-            let saludo = document.getElementById("saludo");
-            saludo.innerHTML = "Bienvenido";
-        } else {
-            alert("Lo sentimos, no podés ingresar");
-        }
-    }
-
-    function usuarioRegistrado() {
-        let registro = prompt("¿Estás registrado?").toUpperCase();
-
-        if (registro === "SI") {
-            let usuario = prompt("Ingresá tu nombre de usuario").toUpperCase();
-            let bienvenida = document.getElementById("tituloContacto");
-            bienvenida.innerHTML = usuario + " dejanos tu mensaje";
-        } else {
-            alert ("Podés registrarte para contactarnos");
-        }
-    }
-
-    validarEdad()
-    usuarioRegistrado();
-    // simuladorCompra()
-    // buscador()
-    // filtro()
-
+    });
+} catch (e) {
+    console.log(e);
+}
 
 
 
